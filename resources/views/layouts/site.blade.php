b39f79677deeb2990f3f984e859caf621212e0b2
gleberphant <cartoladosama@gmail.com>
2022-07-05 20:49:24 -0300

    site head

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -1,56 +1,14 @@
 
-
+@include('layouts.head')
 @include('layouts.nav')
 @include('layouts.footer')
 
 <!DOCTYPE HTML>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="pt-br">
 <head>
-    <title>CJ2 - @yield('title') </title>
-    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
-    <meta charset="utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <meta name="theme-color" content="Blue"/>
-    <meta charset="UTF-8"/>
-    <meta name="description" content="Sistema do Centro Jurídico Janayna Nunes "/>
-    <meta name="author" itemprop="MMPX"/>
 
-    <!-- Adicionar Favicon em todas as versões -->
-    <link rel="shortcut icon" href="#" type="image/x-icon">
-    <link rel="icon" href="#" type="image/x-icon">
+@yield('head')
 
-    <!-- Tags facebook -->
-    <meta property="og:locale" content="pt_BR"/>
-    <meta property="og:url" content="virundum"/>
-    <meta property="og:title"
-          content="Vortexs sunt byssuss de placidus visus. Potus diligenter ducunt ad alter navis. ">
-    <meta property="og:site_name" content="Candidatus ">
-    <meta property="og:description"
-          content="Ubi est albus pars? Cum mortem favere, omnes habitioes promissio grandis, dexter elevatuses. ">
-    <meta property="og:type" content="website"/>
-
-    <!-- Links & Scripts -->
-    <link rel="stylesheet" href="https://use.typekit.net/nbc5nyh.css">
-    <link rel="stylesheet" href="{{asset('/css/app.css')}}"/>
-    @toastr_css
-
-
-    <link id="favicon" rel="shortcut icon" href="{{asset('images/Frame.svg')}}" sizes="16x16" type="image/svg">
-    <link id="favicon" rel="shortcut icon" href="{{asset('images/Frame.svg')}}" sizes="32x32" type="image/svg">
-    <link id="favicon" rel="shortcut icon" href="{{asset('images/Frame.svg')}}" sizes="48x48" type="image/svg">
-
-    <link rel="apple-touch-icon" sizes="48x48" href="{{asset('images/Frame.svg')}}">
-    <link rel="apple-touch-icon" sizes="72x72" href="{{asset('images/Frame.svg')}}">
-    <link rel="apple-touch-icon" sizes="96x96" href="{{asset('images/Frame.svg')}}">
-    <link rel="apple-touch-icon" sizes="144x144" href="{{asset('images/Frame.svg')}}">
-    <link rel="apple-touch-icon" sizes="192x192" href="{{asset('images/Frame.svg')}}">
-    <link rel="apple-touch-icon" sizes="256x256" href="{{asset('images/Frame.svg')}}">
-    <link rel="apple-touch-icon" sizes="384x384" href="{{asset('images/Frame.svg')}}">
-    <link rel="apple-touch-icon" sizes="512x512" href="{{asset('images/Frame.svg')}}">
-
-    <!-- Lightbox -->
-    <link rel="stylesheet" type="text/css" href="{{asset('lightbox/css/lightbox.css')}}"/>
 </head>
 
 
@@ -62,7 +20,6 @@
 @yield('content')
 
 @yield('footer')
-
 
 
 </body>
